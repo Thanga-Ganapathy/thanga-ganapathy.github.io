629fe5ffe329b4dfe413dd337dd6fd076b59f330
Thanga Ganapathy <ganapathy888@gmail.com>
2021-07-11 21:00:39 +0530

    Updates

---
+++ static/js/main.daf41827.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpthanga-ganapathy.github.io"]=this["webpackJsonpthanga-ganapathy.github.io"]||[]).push([[0],{9:function(t,a,e){"use strict";e.r(a);var n=e(1),c=e.n(n),i=e(3),h=e.n(i),r=e(0);var s=function(){return Object(r.jsx)("div",{children:"\ud83d\udc4b Hi, I\u2019m @Thanga-Ganapathy, Website WIP, Please check back later"})};h.a.render(Object(r.jsx)(c.a.StrictMode,{children:Object(r.jsx)(s,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.daf41827.chunk.js.map
